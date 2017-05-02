0ca51b91cbb9159480837f68bbfc13ed28f790bb
GarenLiang <ideabreeze@gmail.com>
2017-05-01 18:50:24 -0700

    Core Data

--- Todo List/ViewController.swift
+++ Todo List/ViewController.swift
@@ -10,10 +10,20 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var importantCheckbox: NSButton!
+    @IBOutlet weak var textField: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func addClicked(_ sender: AnyObject) {
+        if textField.stringValue != "" {
+            if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+                let toDoItem = ToDoItem(context: context)
+                toDoItem.
+            }
+        }
     }
 
     override var representedObject: Any? {
