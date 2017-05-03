43c0b8c1f9f7d45e793fe1e84400a7f1a35d8c05
GarenLiang <ideabreeze@gmail.com>
2017-05-02 17:21:43 -0700

    Save Data :cyclone:

--- Todo List/ViewController.swift
+++ Todo List/ViewController.swift
@@ -12,16 +12,47 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var importantCheckbox: NSButton!
     @IBOutlet weak var textField: NSTextField!
+    var toDoItems : [ToDoItem] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getToDoItems()
+    }
+    func getToDoItems() {
+        //get the todoItems from coredata
+        if let context = (NSApplication.shared().delegate as?
+            AppDelegate)?.persistentContainer.viewContext {
+            do {
+                //set them to class property
+                toDoItems = try context.fetch(ToDoItem.fetchRequest())
+                print(toDoItems.count)
+            } catch {
+            
+            }
+        }
+        //update table
     }
     @IBAction func addClicked(_ sender: AnyObject) {
         if textField.stringValue != "" {
-            if let context = (NSApplication.shared().delegate as? AppDelegate)?.managedObjectContext {
+            if let context = (NSApplication.shared().delegate as?
+                AppDelegate)?.persistentContainer.viewContext {
                 let toDoItem = ToDoItem(context: context)
-                toDoItem.
+                
+                toDoItem.name = textField.stringValue
+                if importantCheckbox.state == 0 {
+                    //not important
+                    toDoItem.important = false
+                } else {
+                    //important
+                    toDoItem.important = true
+                }
+                (NSApplication.shared().delegate as?
+                    AppDelegate)?.saveAction(nil)
+                textField.stringValue = ""
+                importantCheckbox.state = 0
+                
+                getToDoItems()
             }
         }
     }
